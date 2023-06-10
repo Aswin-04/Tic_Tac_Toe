594a6d92555abf3cb3cbbdb1aa009b19b6de4331
Aswin <e.aswin004@gmail.com>
2023-06-10 13:51:17 +0530

    Created user_prompt()

---
+++ main.py
@@ -0,0 +1,65 @@
+#                                   Tic_Tac_Toe 
+
+#-> Prompting the user to press any key/q.
+
+#-> Displaying the empty table filled with - .
+
+#-> Getting Players names as input.
+
+#-> Switch player.
+
+#-> Getting Input from player 1.
+
+#-> Checking for win/draw.
+
+#-> Update and display the table.
+
+#-> Switch player.
+
+#-> Checking for win/draw.
+
+
+def user_prompt():
+    """ Function prompting the user to press any key to start and q to quit """
+
+    char = input("Press any key to start / q to quit. ")
+    if char == "q":
+        quit()
+    else:
+        pass
+
+def get_name():
+    """ Getting Players names as input. """
+    pass
+
+def switch():
+    """" Switching players. """
+    pass
+
+def get_Input1():
+    """ Getting Input from player 1. """
+    pass
+
+def check_win_draw():
+    """ Checking for win/draw. """
+    pass
+
+def disp_table():
+    """ Update and display the table. """
+    pass
+
+
+
+
+
+
+
+""" Main function"""
+def main():
+
+    user_prompt()
+
+
+
+if __name__ == "__main__":
+    main()
