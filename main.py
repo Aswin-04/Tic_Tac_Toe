2eb848b1db548ac054bca2ac5e2de71bb48fbc33
Aswin <e.aswin004@gmail.com>
2023-06-10 20:11:14 +0530

    Completed Tic_Tac_Toe Project --Version 1.0

--- main.py
+++ main.py
@@ -1,47 +1,37 @@
 #                                                     Tic_Tac_Toe 
 
-#-> Prompting the user to press any key/q.
-
-#-> Displaying the empty table filled with - .
-
-#-> Getting Players names as input.
-
-#-> Switch player.
-
-#-> Getting Input from player 1.
-
-#-> Checking for win/draw.
-
-#-> Update and display the table.
-
-#-> Switch player.
-
-#-> Checking for win/draw.
-
 import sys
 
+"Size of the table"
 SIZE = 3
 
+"List for storing the contents of the table"
 List = ["-", "-", "-", "-", "-", "-", "-", "-", "-"]
+
+"List which stores the names of the players"
 List1 = []
+
+"List which stores the Input values to avoid repitition"
 List2 = []
+
+"Counts the total no.of inputs to exit the program when match is in tie"
 draw_count = []
 
-        #-----------------------------------------------------------------------------------------------------------#
+        #-------------------------------------------------------------------#
 
 def user_prompt():
-    """ Function prompting the user to press any key to start and q to quit """
+    " Function prompting the user to press any key to start and q to quit "
 
-    char = input("Press any key to start / q to quit. ")
+    char = input("Press any key to continue / q to quit. ")
     if char == "q":
-        quit()
+        sys.exit()
     else:
         pass
 
-        #-----------------------------------------------------------------------------------------------------------#
+        #-------------------------------------------------------------------#
 
 def get_name(p):
-    """ Getting Players names as input. """
+    " Getting Players names as input. "
 
     while True:
         player_name = input(f"Enter {p}'s name: ").strip().capitalize()
@@ -52,17 +42,17 @@ def get_name(p):
         else:
             print("Please enter a valid name.")
 
-        #-----------------------------------------------------------------------------------------------------------#
+        #-------------------------------------------------------------------#
 
 def switch(player):
-    """" Switching players. """
+    " Switching players. "
 
     if player ==  List1[0]:
         return "X"
     else:
         return "O"
     
-        #-----------------------------------------------------------------------------------------------------------#
+        #-------------------------------------------------------------------#
 
     
 def get_Input(player_0):
@@ -87,13 +77,13 @@ def get_Input(player_0):
         except:
             print("Invalid input! ")
 
-        #-----------------------------------------------------------------------------------------------------------#
+        #-------------------------------------------------------------------#
 
     
-def check_win_or_draw():
-    """ Checking for win/draw. """
+def check_Win_or_Tie():
+    " Checking for Win/Tie. "
 
-    #checking horizontally for a match
+    "checking horizontally for a match"
     count = 0
     for i in range(SIZE):
         if List[count] == "-" or List[count+1] == "-" or List[count+2] == "-":
@@ -113,7 +103,7 @@ def check_win_or_draw():
                     sys.exit("Game over.")                
 
                 
-    #checking vertically for a match
+    "checking vertically for a match"
     count = 0
     for i in range(SIZE):
         if List[count] == "-" or List[count+SIZE] == "-" or List[count+SIZE*2] == "-":
@@ -130,7 +120,7 @@ def check_win_or_draw():
                     print(f"{List1[1]} wins the game")
                     sys.exit("Game over.")                
 
-    #checking diagonally for a match
+    "checking diagonally for a match"
     Mid_val = 4
     for i in range(SIZE):
         if List[Mid_val] == "-":
@@ -153,16 +143,16 @@ def check_win_or_draw():
                     sys.exit("Game over.")
                 
     draw_count.append(1)
-    if len(draw_count) == 8:
+    if len(draw_count) >= 8:
         print("Its a Tie :( ")
         sys.exit("Game over.")
     else:
         print(end = "")
 
-        #-----------------------------------------------------------------------------------------------------------#
+        #-------------------------------------------------------------------#
 
 def disp_table(current_player = None, Input = 0):
-    """ Update and display the table. """
+    " Update and display the table. "
 
     if Input != 0:
         key = switch(current_player)
@@ -186,30 +176,53 @@ def disp_table(current_player = None, Input = 0):
         else:
             print()
 
-        #-----------------------------------------------------------------------------------------------------------#
+        #-------------------------------------------------------------------#
 
-""" Main function"""
+""" Main function """
 def main():
 
+    " Prompting the user to enter any key to play/ q to quit. "
     user_prompt()
+
+    " Prompting the user to enter the name of player_1 and storing the player's name in p1 "
     p1 = get_name("Player_1")
+
+    " Adding p1 to List1 "
     List1.append(p1)
+
+    " Prompting the user to enter the name of player_2 and storing the player's name in p2 "
     p2 = get_name("Player_2")
+
+    " Adding p2 to List1 "
     List1.append(p2)
+
+    " Displaying empty table "
     disp_table()
 
+    " looping over some statements "
     while True:
-        Input = get_Input(p1)
-        disp_table(p1, Input)
-        check_win_or_draw()
-        Input = get_Input(p2)
-        disp_table(p2, Input)
-        check_win_or_draw()
 
-        #-----------------------------------------------------------------------------------------------------------#
+        "  Getting Input from Player_1 "
+        Input = get_Input(p1)
+
+        " Displaying the table "
+        disp_table(p1, Input)
+
+        " Checking for win or tie "
+        check_Win_or_Tie()
+
+        " Getting Input from Player_2 "
+        Input = get_Input(p2)
+
+        " Displaying the table "
+        disp_table(p2, Input)
+
+        "  Checking for win or tie "
+        check_Win_or_Tie()
+
+        #-------------------------------------------------------------------#
 
 if __name__ == "__main__":
     main()
 
-
-        #--------------------------------------------X The-end X----------------------------------------------------#
+        #---------------------------X The-end X-----------------------------#
